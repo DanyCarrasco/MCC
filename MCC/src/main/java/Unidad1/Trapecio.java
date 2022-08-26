ef19f616c7509da24b5628e59a0ac961b93163fa
DanyCarrasco <danycarrasco077@gmail.com>
2022-08-25 21:26:24 -0300

    Version 1 De Metodos Computacionales para el Calculo

---
+++ MCC/src/main/java/Unidad1/Trapecio.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Unidad1;
+
+import Util.TecladoIn;
+
+/**
+ *
+ * @author casa
+ */
+public class Trapecio {
+    public static double f(double x){
+        return Math.tan(x);
+    }
+    
+    public static void main(String[] args) {
+        double a, b, h, n, num = 0;
+        System.out.println("Ingrese el valor de a: ");
+        a = TecladoIn.readLineDouble();
+        System.out.println("Ingrese el valor de b: ");
+        b = TecladoIn.readLineDouble();
+        System.out.println("Ingrese la cantidad de n intervalos: ");
+        n = TecladoIn.readLineDouble();
+        h = (b-a)/n;
+        for (double i = 1; i <= (n - 1); i++) {
+            num = num + f(a+i);
+        }
+        num = (h/2)*(f(a) + f(b) + (2*num));
+        System.out.println("La integral aproximada de la funcion es: ");
+        System.out.println(num);
+    }
+}
